8be13852ca8e8f8e6918105be92b0fde51ec3d58
ccnmagnoo <crcamposn@gmail.com>
2024-08-23 15:41:44 -0400

    request returning result on curr date

--- models/scrapp.py
+++ models/scrapp.py
@@ -6,17 +6,22 @@ class Dof():
     """api http request handler"""
     _SOURCE = "https://www.diariooficial.interior.gob.cl/"
     
-    def _url(self,date=datetime.now()):
+    def _url(self,date:datetime):
         """compose url data request """
-        date_api:str = date.strftime("%d-%m-%Y")
+        date_api:str = date.strftime('%d-%m')
         compose:str = f'{self._SOURCE}?date={date_api}'
         return compose
     
-    def request(self,date:datetime|None):
+    def request(self,date:datetime = datetime.now()):
         """url request"""
         url = self._url(date)
         req = requests.get(url,timeout=1000)
         if req.status_code == 200:
             return req.content
         else:
-         raise ValueError("wrong http request:",req.status_code)
+            raise ValueError("wrong http request:",req.status_code)
+
+if __name__ == '__main__':
+    dof = Dof()
+    req = dof.request()
+    print(req)
